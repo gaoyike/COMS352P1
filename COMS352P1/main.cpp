faf909b0d5b236b18ac824f25484432980166c8e
Chenguang He <gaoyike@gmail.com>
2011-10-26 20:12:07 -0500

    Initial Commit

---
+++ COMS352P1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  COMS352P1
+//
+//  Created by Chenguang He on 10/26/11.
+//  Copyright 2011 non. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
